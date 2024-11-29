acafbe8c73449993f5b3f072c2a2270429a20197
kapibara001 <136171131+kapibara001@users.noreply.github.com>
2024-11-29 20:43:26 +0500

    update

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ x = int(input("Точка X: "))
 y = int(input("Точка Y: "))
 
 def check(x, y, A, R):
-    if (x in range(-(A//2), (A//2)+1)) and (y in range(-(A//2), (A//2)+1)):
-        if (x-(A//2))**2 + (y-(A//2))**2 == R**2:
+    if (x in range(-1*(A//2), (A//2)+1)) and (y in range(-1*(A//2), (A//2)+1)):
+        if ((x-(A//2))**2 + (y+(A//2))**2) <= R**2:
             print(__file__)
             return 0
         else:
@@ -13,6 +13,7 @@ def check(x, y, A, R):
             return 1
     else:
         print("asad")
+        return 0
 
 if check(x, y, A, R):
     print("Точка принадлежит заштрихованной области")
