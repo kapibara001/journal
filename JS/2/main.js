f6f905de201a6ba82fedb7807984fecb95406b8c
kapibara001 <136171131+kapibara001@users.noreply.github.com>
2025-01-09 21:54:46 +0500

    new

---
+++ JS/2/main.js
@@ -0,0 +1,16 @@
+let namee = prompt("1.Введите ваше имя")
+alert(`Hello, ${namee}`);
+//alert("Hello " + namee);
+
+let born_year = prompt("2.Введите ваш год рождения");
+let age = 2024 - born_year;
+alert("Вам " + age + " лет.")
+
+let a = prompt("3.Введите сторону квадрата");
+let aaaa = a*4;
+alert("Периметр квадрата: " + aaaa);
+
+let rad = prompt("4.Введите радиус окружности");
+let sqrad = Math.PI * rad**2;
+alert("Площадь окружности: " + sqrad);
+
